5fc414386074ec9582b2ed157939ffbf9ef5cc3d
Egor Gavrilenko <bambloo769@gmail.com>
2023-07-09 03:37:20 +0300

    feat: add commands enums

---
+++ src/ws_server/commands/ServerCommands.ts
@@ -0,0 +1,10 @@
+export enum ServerCommands {
+  REG = 'reg',
+  UPDATE_WINNERS = 'update_winners',
+  UPDATE_ROOM = 'update_room',
+  CREATE_GAME = 'create_game',
+  START_GAME = 'start_game',
+  ATTACK = 'attack',
+  TURN = 'turn',
+  FINISH = 'finish',
+}
